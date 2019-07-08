2c33a21020fb10f43dc5cc12a4e238f40242d1f0
Ian Sloat <iansloat537@gmail.com>
2019-07-07 22:57:51 -0500

    Thiccbot now safe for deployment
    Default permissions are set on join

--- THICCBot4J/src/main/IanSloat/thiccbot/events/GuildJoin.java
+++ THICCBot4J/src/main/IanSloat/thiccbot/events/GuildJoin.java
@@ -1,10 +1,17 @@
 package main.IanSloat.thiccbot.events;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import main.IanSloat.thiccbot.commands.Command;
 import main.IanSloat.thiccbot.tools.GuildSettingsManager;
-
+import main.IanSloat.thiccbot.tools.PermissionsManager;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 
 public class GuildJoin {
@@ -22,6 +29,25 @@ public class GuildJoin {
 			GuildSettingsManager sManager = new GuildSettingsManager(event.getGuild());
 			sManager.CreateSettings();
 			Events.knownGuildIds.add(event.getGuild().getId());
+			PermissionsManager permMgr = Command.getPermissionsManager(event.getGuild());
+			permMgr.clearPermissions();
+			List<Member> guildUsers = new ArrayList<Member>();
+			List<Role> guildRoles = new ArrayList<Role>();
+			guildUsers.addAll(event.getGuild().getMembers());
+			guildRoles.addAll(event.getGuild().getRoles());
+			guildUsers.remove(event.getGuild().getOwner());
+			for (Member user : guildUsers) {
+				if (user.hasPermission(Permission.ADMINISTRATOR) == false) {
+					permMgr.SetPermission(permMgr.INFO, user, permMgr.DENY);
+					permMgr.SetPermission(permMgr.MANAGE_GLOBAL, user, permMgr.DENY_GLOBAL);
+				}
+			}
+			for (Role role : guildRoles) {
+				if (!(role.getPermissions().contains(Permission.ADMINISTRATOR))) {
+					permMgr.SetPermission(permMgr.INFO, role, permMgr.DENY);
+					permMgr.SetPermission(permMgr.MANAGE_GLOBAL, role, permMgr.DENY_GLOBAL);
+				}
+			}
 		}
 	}
 
